6126e0dbce719c0e078abb501954b66a3a9e5973
breno.prata <breno.prata@basis.com.br>
2019-02-12 17:05:31 -0200

    Äjustando config para herokuapp

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist'));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json, authorization');
+    next();
+});
+
+
 app.get('/*', function(req,res) {
 
     res.sendFile(path.join(__dirname,'/dist/index.html'));
